5fcacf59a82c1dbd718ec6a1908804513be167ec
kydesk <kydesk@outlook.com>
2020-11-08 20:25:08 +0800

    Done

---
+++ week10/MyDjango/index/form.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='用户名')
+    password = forms.CharField(label='密码',widget=forms.PasswordInput, min_length=8)
---
+++ week10/MyDjango/index/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
+from .models import Douban
+from .models import Post
+from .form import LoginForm
+
+# Create your views here.
+
+def index(request):
+    comments = Douban.objects.all()
+    return render(request, 'index.html', locals())
+
+def sign_in(request):
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            cd = login_form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user:
+                login(request, user)
+                return redirect('/index')
+            else:
+                return HttpResponse('登录失败')
+
+    if request.method == "GET":
+        login_form = LoginForm()
+        return render(request, 'login.html', {'form': login_form})
